ce5d97f3564b2e9030082b55be2e0fe76793f73c
Mario Michel <mariomichel58@gmail.com>
2026-03-22 10:31:26 -0500

    use sendgrid

--- invitations/models.py
+++ invitations/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 class rsvp(models.Model):
-    name = models.CharField(max_length=150)
+    name = models.CharField(max_length=150, unique=True)
     spouse_name = models.CharField(max_length=100, blank=True, null=True)
     email = models.EmailField(unique=True) # prevents duplicate email
     number = models.CharField(max_length= 20, blank=True, default= '', unique=True)
